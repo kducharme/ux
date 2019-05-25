5b05adf34a75940f90f5f3af15eda4f56ecd28b5
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-05-25 18:26:20 -0400

    nav complete

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,14 @@
   <div id="app">
     <nav>
       <img alt="TaxJar logo" src="./assets/taxjar.svg" class="logo">
-      <router-link to="/products" exact>Products</router-link>
+      <div>
+        <router-link to="/dashboard" exact>Dashboard</router-link>
+        <router-link to="/transactions" exact>Transactions</router-link>
+        <router-link to="/autofile" exact>AutoFile</router-link>
+        <router-link to="/products" exact>Products</router-link>
+        <router-link to="/filing-history" exact>Filing History</router-link>
+        <router-link to="/account" exact>Account</router-link>
+      </div>
     </nav>
     <router-view/>
   </div>
@@ -23,8 +30,32 @@ export default {
 
 <style lang="scss">
 @import "./styles/global.scss";
+@import './styles/mixins.scss';
+@import './styles/variables.scss';
 
-* {
-  margin: 0;
+
+nav {
+  height: 60px;
+  border-bottom: 1px solid $grayBorder;
+  @include display-flex(space-between, center, row);
+  padding: 0 100px;
+
+  .logo {
+    height: 28px;
+    width: auto;
+  }
+  a {
+    color: #7a7b7d;
+    padding-left: 32px;
+    font-weight: $weightLight;
+    font-size: 15px;
+  }
+  a:hover {
+    color: $colorFontDark;
+  }
+  .router-link-active {
+      font-weight: $weightMedium;
+      color: $colorTaxJar;
+    }
 }
 </style>
--- src/components/PrimaryNavigation.vue
+++ src/components/PrimaryNavigation.vue
@@ -1,6 +1,11 @@
 <template>
   <nav>
     <img alt="TaxJar logo" src="../assets/taxjar.svg" class="logo">
+    <router-link @click.native="resetActive" to="/products" exact>Dashboard</router-link>
+    <router-link @click.native="resetActive" to="/products" exact>Products</router-link>
+    <router-link @click.native="resetActive" to="/products" exact>Products</router-link>
+    <router-link @click.native="resetActive" to="/products" exact>Products</router-link>
+    <router-link @click.native="resetActive" to="/products" exact>Products</router-link>
     <router-link @click.native="resetActive" to="/products" exact>Products</router-link>
     <router-view/>
   </nav>
@@ -17,16 +22,5 @@ export default {
 @import "../styles/variables.scss";
 @import "../styles/mixins.scss";
 
-nav {
-  height: 60px;
-  border-bottom: 1px solid $grayBorder;
-  @include display-flex(flex-start, center, row);
-  padding: 0 100px;
-}
-
-.logo {
-  height: 30px;
-  width: auto;
-}
 </style>
 
