e6b2dfd6ab5b6131f5e83032d732f45a2e81c8b5
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-05-25 07:49:14 -0400

    nav added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/taxjar.svg">
+    <PrimaryNavigation />
     <Products />
   </div>
 </template>
 
 <script>
 import Products from './pages/Products.vue'
+import PrimaryNavigation from './components/PrimaryNavigation.vue'
 
 export default {
   name: 'app',
   components: {
-    Products
+    Products,
+    PrimaryNavigation
   }
 }
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/PrimaryNavigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <img alt="TaxJar logo" src="../assets/taxjar.svg" />
+    <router-link @click.native="resetActive" to="/products" exact>Products</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "PrimaryNavigation",
+  components: {}
+};
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+}
+</style>
+
