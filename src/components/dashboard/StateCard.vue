8dac886202fdb4f2fb2d5b75ec62c353c39d085d
Kyle Ducharme <ducharme.kyle@gmail.com>
2019-11-20 08:06:43 -0600

    state cards rendering

---
+++ src/components/dashboard/StateCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card">
+      <div class="card__icon">{{state.name}}</div>
+      <div class="card__name">{{state.name}}</div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "Filters",
+  data() {
+    return {};
+  },
+  props: {
+    state: {
+      type: Object
+    }
+  },
+  methods: {
+
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/variables.scss";
+@import "../../styles/mixins.scss";
+
+.card {
+  border: 1px solid $grayBorder;
+  border-radius: 3px;
+  min-height: 300px;
+  background: white;
+  flex: 0 1 calc(25% - 1em);
+  margin-bottom: 1em;
+  padding: 24px;
+  
+}
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -3,6 +3,9 @@
     <Header :headerContent="this.headerContent" />
     <div class="content">
       <Map />
+      <div class="content__cards">
+        <StateCard v-for="state in allStates" :key="state.id" :state="state" />
+      </div>
     </div>
   </div>
 </template>
@@ -10,10 +13,11 @@
 <script>
 import Header from "../components/Header.vue";
 import Map from "../components/dashboard/Map.vue";
+import StateCard from "../components/dashboard/StateCard.vue";
 
 export default {
   name: "dashboard",
-  components: { Header, Map },
+  components: { Header, Map, StateCard },
   data() {
     return {
       headerContent: {
@@ -21,10 +25,26 @@ export default {
         tabOne: "Overview",
         tabTwo: "State summaries",
         tabThree: "Account activity"
-      }
+      },
+      allStates: []
     };
   },
-  methods: {}
+  methods: {
+    getUserStates: function() {
+      fetch(`http://localhost:3000/states`)
+        .then(r => r.json())
+        .then(states => {
+          this.allStates = states;
+          this.sortStates();
+        });
+    },
+    sortStates() {
+      return this.allStates.sort((a, b) => a.name.localeCompare(b.name));
+    }
+  },
+  mounted() {
+    this.getUserStates();
+  }
 };
 </script>
 
@@ -34,14 +54,20 @@ export default {
 
 .dashboard {
   @include display-flex(flex-start, flex-start, column);
-  height: calc(100vh - 60px);
+  min-height: calc(100vh - 60px);
   width: 100vw;
   .content {
     @include display-flex(flex-start, center, column);
-    padding: 40px 100px 0px;
+    padding: 40px 100px;
     background: $grayBackground;
     min-height: calc(100vh - 175px);
     width: 100%;
+    .content__cards {
+      @include display-flex(space-between, center, row);
+      flex-wrap: wrap;
+      margin-top: 32px;
+      width: 100%;
+    }
   }
 }
 </style>
